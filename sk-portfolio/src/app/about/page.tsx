b889149ff4e48fe6e3e651a04639e2ab06b12414
shelbykauth <shelbykauth@gmail.com>
2025-09-16 02:04:42 -0400

    Rough Out About Me Page

--- sk-portfolio/src/app/about/page.tsx
+++ sk-portfolio/src/app/about/page.tsx
@@ -1,54 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function About() {
   return (
     <>
-      About Page
-      <Image
-        className="dark:invert"
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
-      <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-        <li className="mb-2 tracking-[-.01em]">
-          Get started by editing{" "}
-          <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-            src/app/page.tsx
-          </code>
-          .
-        </li>
-        <li className="tracking-[-.01em]">
-          Save and see your changes instantly.
-        </li>
-      </ol>
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      <h1 className="text-5xl">About Me</h1>
+      <p className="text-lg">
+        Programming runs through my veins.
+        <Link
+          className="underline inline-block mx-1"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener"
+          href="https://www.linkedin.com/in/mariekauth/"
         >
-          <Image
-            className="dark:invert"
-            src="/vercel.svg"
-            alt="Vercel logomark"
-            width={20}
-            height={20}
-          />
-          Deploy now
-        </a>
-        <a
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          (Ask Marie Kauth!)
+        </Link>
+        {`I've spent the better part of 2 decades learning and honing my craft as
+        a software engineer. With JavaScript as my mother tongue, I've explored
+        dozens of languages and frameworks. What I value most highly in
+        applications includes Data Integrity, Security, Accessibility, and
+        Usability. What I value as a person includes Honesty, the other kind of
+        Integrity, and Compassion.`}
+      </p>
+      <p className="text-lg">(Insert more talking points here)</p>
+      <p className="text-lg">
+        {`In my free time, I enjoy Minecraft, CodinGame, playing a dozen different
+        musical instruments (badly), D&D, writing fantasy stories, knitting, and
+        more programming. Sometimes, I even combine hobbies, such as my goal of
+        using Minecraft's Create mod to build a pipe organ, controlled by a
+        ComputerCraft program that reads MIDI files and live input. Besides
+        Minecraft, my favorite video games include`}
+        <Link
+          className="underline inline-block mx-1"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener"
+          href="https://www.mobiusdigitalgames.com/outer-wilds.html"
         >
-          Read our docs
-        </a>
-      </div>
+          Outer Wilds
+        </Link>
+        and
+        <Link
+          className="underline inline-block mx-1"
+          target="_blank"
+          rel="noopener"
+          href="https://www.hollowknight.com/"
+        >
+          Hollow Knight
+        </Link>
+      </p>
     </>
   );
 }
