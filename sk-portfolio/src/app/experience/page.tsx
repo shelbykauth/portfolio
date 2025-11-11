7b7dfad7ed18234fd4662e126588e48b4c5c99b1
shelbykauth <shelbykauth@gmail.com>
2025-11-11 11:38:50 -0500

    Update Experience Page

--- sk-portfolio/src/app/experience/page.tsx
+++ sk-portfolio/src/app/experience/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-import experienceMarkDown from "./experience.md";
-import Markdown from "react-markdown";
-
 export default function Experience() {
   return (
     <>
-      <h1 className="text-5xl mr-auto ml-auto">Work Experience</h1>
+      <h1 className="text-4xl mr-auto ml-auto">Work Experience</h1>
       <table className="[&_td]:p-6">
         <thead>
           <tr className="sr-only">
@@ -16,16 +12,59 @@ export default function Experience() {
         <tbody>
           <tr>
             <td>2018 - 2025</td>
-            <h3>Cynerge Consulting</h3>
-            <p></p>
+            <td>
+              <h3 className="text-2xl">Cynerge Consulting</h3>
+              <ul className=" max-w-xl">
+                <li>
+                  Full-stack Application Developer, primarily contracting for US
+                  Forest Service.
+                </li>
+                <li>
+                  Lead Developer on CIAO - Check In And Out, used for field
+                  safety tracking.
+                </li>
+                <li>
+                  Full-Stack surge support for several major projects across
+                  half a dozen stacks
+                </li>
+                <li>
+                  Agile/Scrum methodologies with daily check-ins and frequent
+                  commits
+                </li>
+              </ul>
+            </td>
           </tr>
           <tr>
             <td>2017 - 2018</td>
-            <td>Adviso, Inc</td>
+            <td>
+              <h3 className="text-2xl">Adviso, Inc</h3>
+              <ul className="max-w-xl">
+                <li>
+                  Junior Programmer, primarily working on frontend features.
+                </li>
+                <li>Created dynamic online pricebook</li>
+              </ul>
+            </td>
           </tr>
           <tr>
-            <td>2007 - 2018</td>
-            <td>Freelance / Apprenticeship</td>
+            <td>2010 - 2018</td>
+            <td>
+              <h3 className="text-2xl">Freelance / OWebPro</h3>
+              <ul className="max-w-xl">
+                <li>
+                  Apprentice Web Developer under Marie Kauth, founder of
+                  OWebPro.
+                </li>
+                <li>
+                  Frontend JavaScript debugging, optimizing SQL Queries, Quality
+                  Assurance.
+                </li>
+                <li>
+                  Independent Study of over 20 languages, a dozen platforms, and
+                  general best practices.
+                </li>
+              </ul>
+            </td>
           </tr>
         </tbody>
       </table>
